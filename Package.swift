c2a3f2a328ae4a11a2f5cd4ef34667b3ed98b0b9
Sharkzuria <baumabulondo@gmail.com>
2023-02-28 00:59:12 +0300

    [build] Setup SPM Package

--- Package.swift
+++ Package.swift
@@ -1,26 +1,28 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "StocksAPI",
+    platforms: [
+        .iOS(.v13), .macOS(.v12), .macCatalyst(.v13), .tvOS(.v13), .watchOS(.v8)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "StocksAPI",
             targets: ["StocksAPI"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .executable(
+            name: "StocksAPIExec",
+            targets: ["StocksAPIExec"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "StocksAPI",
             dependencies: []),
+        .executableTarget(
+            name: "StocksAPIExec",
+            dependencies: ["StocksAPI"]),
         .testTarget(
             name: "StocksAPITests",
             dependencies: ["StocksAPI"]),
