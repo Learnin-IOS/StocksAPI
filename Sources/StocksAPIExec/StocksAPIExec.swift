da68bbb162d1b5ac6827d85dab9791c0bd7efe02
Sharkzuria <baumabulondo@gmail.com>
2023-03-01 06:30:33 +0300

    [feat] Add Fetch Charts API Models  & JSON Encoding

--- Sources/StocksAPIExec/StocksAPIExec.swift
+++ Sources/StocksAPIExec/StocksAPIExec.swift
@@ -17,8 +17,12 @@ struct StocksAPIExec {
 //        let quoteResponse = try! JSONDecoder().decode(QuoteResponse.self, from: data)
 //        print(quoteResponse)
        
-        let (searchData, _) = try! await URLSession.shared.data(from: URL(string: "https://query1.finance.yahoo.com/v1/finance/search?q=APPLE")!)
-        let searchResponse = try! JSONDecoder().decode(SearchTickerResponse.self, from: searchData)
-        print(searchResponse)
+//        let (searchData, _) = try! await URLSession.shared.data(from: URL(string: "https://query1.finance.yahoo.com/v1/finance/search?q=APPLE")!)
+//        let searchResponse = try! JSONDecoder().decode(SearchTickerResponse.self, from: searchData)
+//        print(searchResponse)
+//        
+        let (chartData, _) = try! await URLSession.shared.data(from: URL(string: "https://query1.finance.yahoo.com/v8/finance/chart/AAPL?range=1d&interval=1m&indicators=quote&includeTimestamps=true")!)
+        let chartResponse = try! JSONDecoder().decode(ChartResponse.self, from: chartData)
+        print(chartResponse)
     }
 }
