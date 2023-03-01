8b62c50b2eca93aa23e90290d9955853f51c9f2f
Sharkzuria <baumabulondo@gmail.com>
2023-03-01 06:33:29 +0300

    [refactor] refractor StocksAPIExec

--- Sources/StocksAPIExec/StocksAPIExec.swift
+++ Sources/StocksAPIExec/StocksAPIExec.swift
@@ -12,15 +12,15 @@ import StocksAPI
 struct StocksAPIExec {
     
     static func main() async {
-//        let (data, _) = try! await URLSession.shared.data(from: URL(string: "https://query1.finance.yahoo.com/v7/finance/quote?")!)
-//        
-//        let quoteResponse = try! JSONDecoder().decode(QuoteResponse.self, from: data)
-//        print(quoteResponse)
+        let (data, _) = try! await URLSession.shared.data(from: URL(string: "https://query1.finance.yahoo.com/v7/finance/quote?")!)
+        
+        let quoteResponse = try! JSONDecoder().decode(QuoteResponse.self, from: data)
+        print(quoteResponse)
        
-//        let (searchData, _) = try! await URLSession.shared.data(from: URL(string: "https://query1.finance.yahoo.com/v1/finance/search?q=APPLE")!)
-//        let searchResponse = try! JSONDecoder().decode(SearchTickerResponse.self, from: searchData)
-//        print(searchResponse)
-//        
+        let (searchData, _) = try! await URLSession.shared.data(from: URL(string: "https://query1.finance.yahoo.com/v1/finance/search?q=APPLE")!)
+        let searchResponse = try! JSONDecoder().decode(SearchTickerResponse.self, from: searchData)
+        print(searchResponse)
+        
         let (chartData, _) = try! await URLSession.shared.data(from: URL(string: "https://query1.finance.yahoo.com/v8/finance/chart/AAPL?range=1d&interval=1m&indicators=quote&includeTimestamps=true")!)
         let chartResponse = try! JSONDecoder().decode(ChartResponse.self, from: chartData)
         print(chartResponse)
