3d4a917b8c66d1753b62bf5cabe27bc196343683
Sharkzuria <baumabulondo@gmail.com>
2023-03-03 01:51:57 +0300

    [feat] implement Fetch Charts API

--- Sources/StocksAPIExec/StocksAPIExec.swift
+++ Sources/StocksAPIExec/StocksAPIExec.swift
@@ -15,11 +15,15 @@ struct StocksAPIExec {
     
     static func main() async {
         do {
-            let quotes = try await stocksAPI.fetchQuotes(symbols: "AAPL,MSFT,GOOG,TSLA")
-            print(quotes)
-            
-            let tickers = try await stocksAPI.searchTickers(query: "tesla")
-            print(tickers)
+            //            let quotes = try await stocksAPI.fetchQuotes(symbols: "AAPL,MSFT,GOOG,TSLA")
+            //            print(quotes)
+            //
+            //            let tickers = try await stocksAPI.searchTickers(query: "tesla")
+            //            print(tickers)
+            //
+            if let chart = try await stocksAPI.fetchChartData(symbol: "AAPL", range: .oneDay) {
+                print(chart)
+            }
         } catch {
             print(error.localizedDescription)
         }
