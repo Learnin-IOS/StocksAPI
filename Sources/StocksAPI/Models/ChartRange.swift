825c20a9036f76cf27d23e7e07600c84270ab99c
Sharkzuria <baumabulondo@gmail.com>
2023-03-03 00:08:04 +0300

    [chore] Add Chart Range Data

---
+++ Sources/StocksAPI/Models/ChartRange.swift
@@ -0,0 +1,33 @@
+//
+//  ChartRange.swift
+//  
+//
+//  Created by Le Bon B' Bauma on 02/03/2023.
+//
+
+import Foundation
+
+
+public enum ChartRange: String, CaseIterable {
+    case oneDay = "1d"
+    case oneWeek = "5d"
+    case oneMonth = "1mo"
+    case threeMonth = "3mo"
+    case sixMonth = "6mo"
+    case ytd
+    case oneYear = "1y"
+    case twoYear = "2y"
+    case fiveYear = "5y"
+    case tenYear = "10y"
+    case max
+    
+    public var interval: String {
+        switch self {
+        case .oneDay: return "1m"
+        case .oneWeek: return "5m"
+        case .oneMonth: return "90m"
+        case .threeMonth, .sixMonth,.ytd, .oneYear, .twoYear: return "1d"
+        case .fiveYear, .tenYear: return "1wk"
+        case .max: return "3mo"
+    }
+}
