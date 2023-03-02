04fd3ac32c7dc5c3bd997297409ceb71b2f2afdc
Sharkzuria <baumabulondo@gmail.com>
2023-03-02 22:18:17 +0300

    [feat] Implement Fetch Quotes API

--- Sources/StocksAPIExec/StocksAPIExec.swift
+++ Sources/StocksAPIExec/StocksAPIExec.swift
@@ -11,18 +11,15 @@ import StocksAPI
 @main
 struct StocksAPIExec {
     
+    static let stocksAPI = StocksAPI()
+    
     static func main() async {
-        let (data, _) = try! await URLSession.shared.data(from: URL(string: "https://query1.finance.yahoo.com/v7/finance/quote?")!)
-        
-        let quoteResponse = try! JSONDecoder().decode(QuoteResponse.self, from: data)
-        print(quoteResponse)
-       
-        let (searchData, _) = try! await URLSession.shared.data(from: URL(string: "https://query1.finance.yahoo.com/v1/finance/search?q=APPLE")!)
-        let searchResponse = try! JSONDecoder().decode(SearchTickerResponse.self, from: searchData)
-        print(searchResponse)
-        
-        let (chartData, _) = try! await URLSession.shared.data(from: URL(string: "https://query1.finance.yahoo.com/v8/finance/chart/AAPL?range=1d&interval=1m&indicators=quote&includeTimestamps=true")!)
-        let chartResponse = try! JSONDecoder().decode(ChartResponse.self, from: chartData)
-        print(chartResponse)
+        do {
+            let quotes = try await stocksAPI.fetchQuotes(symbols: "AAPL,MSFT,GOOG,TSLA")
+            print(quotes)
+        } catch {
+            print(error.localizedDescription)
+        }
+     
     }
 }
