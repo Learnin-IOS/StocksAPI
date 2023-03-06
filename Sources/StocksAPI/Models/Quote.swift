201fddbc7bb4360c0452d049441bde00fce40b06
Sharkzuria <baumabulondo@gmail.com>
2023-03-06 08:19:24 +0300

    [fix] Updater Value of optional type 'String?' that must be unwrapped to a value of type 'String'

--- Sources/StocksAPI/Models/Quote.swift
+++ Sources/StocksAPI/Models/Quote.swift
@@ -40,15 +40,20 @@ public struct Quote: Codable, Identifiable, Hashable {
     
     public let id = UUID()
     
+ 
+    public let symbol: String
+
     public let currency: String?
     public let marketState: String?
+    
     public let fullExchangeName: String?
     public let displayName: String?
-    public let symbol: String?
+    
     public let regularMarketPrice: Double?
     public let regularMarketChange: Double?
     public let regularMarketChangePercent: Double?
-    public let regularMarketPreviousClose: Double?
+    public let regularMarketChangePreviousClose: Double?
+    public let regularMarketTime: Date?
     
     public let postMarketPrice: Double?
     public let postMarketChange: Double?
